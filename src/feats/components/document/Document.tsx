92fa9c2947626ea50beeda21e2756b754b054dcd
triton <rayntriton@gmail.com>
2025-05-07 13:49:05 -0600

    more code 2025-05-06

--- src/feats/components/document/Document.tsx
+++ src/feats/components/document/Document.tsx
@@ -25,89 +25,102 @@ export function Document(){
   const getColor = ( index:number ) => index % 2 == 0 ? evenClass : oddClass
 
   return (
-    <Switch>
-      <Match when = { loading() } >
-        <span>Loading...</span>
-      </Match>
-      <Match when = { error() } >
-        <span>Error: { error() }</span>
-      </Match>
-      <Match when={ response() }>{ ( () => {
+    <div>
+      <div class = 'flex flex-wrap'>
+        <div>{ ( () => {
           const document = response()?.result!
-          let docutmentType = document.documentType
-          
           return (
-             <Show
-                  when = { global.isPortableDevice() }
-                  fallback = { <>
-                    <table class = { `w-ful min-w-full` } >
-                      { document.items.map( ( item, index ) => {
-                        const Flags = <>
-                            { item.disabled ? <GlyphRedFlag class = 'glyph-shadow-black' /> : <></> }
-                            { item.raincheck ? <GlyphPurpleFlag class = 'glyph-shadow-black' /> : <></> }
-                            { item.edited ? <GlyphYellowFlag class = 'glyph-shadow-black' /> : <></> }
-                          </>
-                        const row =
-                          <tr class = { getColor( index ) } >
-                            <td class = 'bg-opacity-50 rounded-md flex flex-row flex-shrink w-11' >
-                              { Flags }</td>
-                            <td class = 'w-14 m-1 p-1 bg-indigo-800 bg-opacity-50 rounded-md' >
-                              <div>{ item.quantity }</div></td>
-                            <td class = 'm-1 p-1 bg-indigo-950 bg-opacity-50 rounded-md' >
-                              <div>{ item.unit }</div></td>
-                            <td class = 'm-1 p-1 bg-indigo-800 bg-opacity-50 rounded-md' >
-                              <div>{ item.code }</div></td>
-                            <td class = 'm-1 p-1 bg-indigo-950 bg-opacity-50 rounded-md' >
-                              <div>{ item.description } </div></td>
-                            <td class = 'm-1 p-1 bg-indigo-800 bg-opacity-50 rounded-md' >
-                              <div>{ item.unitCode } </div></td>
-                            <td class = 'm-1 p-1 bg-indigo-950 bg-opacity-50 rounded-md' >
-                              <div>{ item.prodservCode } </div></td>
-                            <td class = 'w-14 m-1 p-1 bg-indigo-800 bg-opacity-50 rounded-md' >
-                              <div>{ round2( item.unitPriceComputed | item.unitPrice ) } </div></td>
-                            <td class = 'w-14 m-1 p-1 bg-indigo-950 bg-opacity-50 rounded-md' >
-                              <div>{ round2( ( item.unitPriceComputed | item.unitPrice ) * item.quantity ) } </div></td>
-                          </tr>
-                        return row
-                       } ) }
-                    </table>
-                    
-                  </> } >
-                  <div class = { `w-full` } >
-                  { document.items.map( ( item, index ) => {
-                    const Flags = <>
-                        { item.disabled ? <GlyphRedFlag class = 'glyph-shadow-black' /> : <></> }
-                        { item.raincheck ? <GlyphPurpleFlag class = 'glyph-shadow-black' /> : <></> }
-                        { item.edited ? <GlyphYellowFlag class = 'glyph-shadow-black' /> : <></> }
-                      </>
-                    return (
-                      <div class = { `flex flex-wrap ${ getColor( index ) }` } data-index = { index }>
+            <Show when = { response() }>
+              <Show when = { response() }>
+              </Show>
+            </Show>
+          ) } )() }
+        </div>
+      </div>
+      <Switch>
+        <Match when = { loading() } >
+          <span>Loading...</span>
+        </Match>
+        <Match when = { error() } >
+          <span>Error: { error() }</span>
+        </Match>
+        <Match when={ response() }>{ ( () => {
+            const document = response()?.result!
+            let docutmentType = document.documentType
+            
+            return (
+              <Show
+                    when = { global.isPortableDevice() }
+                    fallback = { <>
+                      <table class = { `w-ful min-w-full` } >
+                        { document.items.map( ( item, index ) => {
+                          const Flags = <>
+                              { item.disabled ? <GlyphRedFlag class = 'glyph-shadow-black' /> : <></> }
+                              { item.raincheck ? <GlyphPurpleFlag class = 'glyph-shadow-black' /> : <></> }
+                              { item.edited ? <GlyphYellowFlag class = 'glyph-shadow-black' /> : <></> }
+                            </>
+                          const row =
+                            <tr class = { getColor( index ) } >
+                              <td class = 'bg-opacity-50 rounded-md flex flex-row flex-shrink w-11' >
+                                { Flags }</td>
+                              <td class = 'w-14 m-1 p-1 bg-indigo-800 bg-opacity-50 rounded-md' >
+                                <div>{ item.quantity }</div></td>
+                              <td class = 'm-1 p-1 bg-indigo-950 bg-opacity-50 rounded-md' >
+                                <div>{ item.unit }</div></td>
+                              <td class = 'm-1 p-1 bg-indigo-800 bg-opacity-50 rounded-md' >
+                                <div>{ item.code }</div></td>
+                              <td class = 'm-1 p-1 bg-indigo-950 bg-opacity-50 rounded-md' >
+                                <div>{ item.description } </div></td>
+                              <td class = 'm-1 p-1 bg-indigo-800 bg-opacity-50 rounded-md' >
+                                <div>{ item.unitCode } </div></td>
+                              <td class = 'm-1 p-1 bg-indigo-950 bg-opacity-50 rounded-md' >
+                                <div>{ item.prodservCode } </div></td>
+                              <td class = 'w-14 m-1 p-1 bg-indigo-800 bg-opacity-50 rounded-md' >
+                                <div>{ round2( item.unitPriceComputed | item.unitPrice ) } </div></td>
+                              <td class = 'w-14 m-1 p-1 bg-indigo-950 bg-opacity-50 rounded-md' >
+                                <div>{ round2( ( item.unitPriceComputed | item.unitPrice ) * item.quantity ) } </div></td>
+                            </tr>
+                          return row
+                        } ) }
+                      </table>
                       
-                        <div class = 'bg-opacity-50 rounded-md flex flex-row flex-shrink' >
-                          { Flags }</div>
-                        <div class = 'w-14 m-1 p-1 bg-indigo-800 bg-opacity-50 rounded-md' >
-                          <div>{ item.quantity }</div></div>
-                        <div class = 'm-1 p-1 bg-indigo-950 bg-opacity-50 rounded-md' >
-                          <div>{ item.unit } </div></div>
-                        <div class = 'm-1 p-1 bg-indigo-800 bg-opacity-50 rounded-md' >
-                          <div>{ item.code } </div></div>
-                        <div class = 'm-1 p-1 bg-indigo-950 bg-opacity-50 rounded-md' >
-                          <div>{ item.description } </div></div>
-                        <div class = 'm-1 p-1 bg-indigo-800 bg-opacity-50 rounded-md' >
-                          <div>{ item.unitCode } </div></div>
-                        <div class = 'm-1 p-1 bg-indigo-950 bg-opacity-50 rounded-md' >
-                          <div>{ item.prodservCode } </div></div>
-                        <div class = 'w-14 m-1 p-1 bg-indigo-800 bg-opacity-50 rounded-md' >
-                          <div>{ round2( item.unitPriceComputed ) } </div></div>
-                        <div class = 'w-14 m-1 p-1 bg-indigo-950 bg-opacity-50 rounded-md' >
-                          <div>{ round2( item.unitPriceComputed * item.quantity ) }</div></div>
-                      </div>
-                  ) } ) }
-                </div>
-                </Show>
-          ) } )()
-        }
-      </Match>
-  </Switch>
+                    </> } >
+                    <div class = { `w-full` } >
+                    { document.items.map( ( item, index ) => {
+                      const Flags = <>
+                          { item.disabled ? <GlyphRedFlag class = 'glyph-shadow-black' /> : <></> }
+                          { item.raincheck ? <GlyphPurpleFlag class = 'glyph-shadow-black' /> : <></> }
+                          { item.edited ? <GlyphYellowFlag class = 'glyph-shadow-black' /> : <></> }
+                        </>
+                      return (
+                        <div class = { `flex flex-wrap ${ getColor( index ) }` } data-index = { index }>
+                        
+                          <div class = 'bg-opacity-50 rounded-md flex flex-row flex-shrink' >
+                            { Flags }</div>
+                          <div class = 'w-14 m-1 p-1 bg-indigo-800 bg-opacity-50 rounded-md' >
+                            <div>{ item.quantity }</div></div>
+                          <div class = 'm-1 p-1 bg-indigo-950 bg-opacity-50 rounded-md' >
+                            <div>{ item.unit } </div></div>
+                          <div class = 'm-1 p-1 bg-indigo-800 bg-opacity-50 rounded-md' >
+                            <div>{ item.code } </div></div>
+                          <div class = 'm-1 p-1 bg-indigo-950 bg-opacity-50 rounded-md' >
+                            <div>{ item.description } </div></div>
+                          <div class = 'm-1 p-1 bg-indigo-800 bg-opacity-50 rounded-md' >
+                            <div>{ item.unitCode } </div></div>
+                          <div class = 'm-1 p-1 bg-indigo-950 bg-opacity-50 rounded-md' >
+                            <div>{ item.prodservCode } </div></div>
+                          <div class = 'w-14 m-1 p-1 bg-indigo-800 bg-opacity-50 rounded-md' >
+                            <div>{ round2( item.unitPriceComputed ) } </div></div>
+                          <div class = 'w-14 m-1 p-1 bg-indigo-950 bg-opacity-50 rounded-md' >
+                            <div>{ round2( item.unitPriceComputed * item.quantity ) }</div></div>
+                        </div>
+                    ) } ) }
+                  </div>
+                  </Show>
+            ) } )()
+          }
+        </Match>
+    </Switch>
+  </div>
   )
 }
